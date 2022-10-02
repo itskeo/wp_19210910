a47abf9af5c9288907dcbaa69fa6f2546d96bd6d
itskeo <keona.adhitya@gmail.com>
2022-10-02 20:11:38 +0700

    Tugas Pertemuan 3

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    Public function index()
+    {
+        echo "<h3>Perkenalkan<h3>" ;
+        echo"Nama Saya Keona Adhitya
+            Saya tinggal di daerah Pamulang 2
+            olahraga yang saya sukai adalah Badminton" ;
+    }
+}
+?>
